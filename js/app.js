01b28298977318182e5c2ea4ac51c26df1e146bc
kajal-2011 <kajalrekha888@gmail.com>
2024-02-20 22:35:04 +0600

    lenis added

--- js/app.js
+++ js/app.js
@@ -93,3 +93,20 @@ $(".owl-carousel").owlCarousel({
     },
   },
 });
+
+// smooth scroll
+
+const lenis = new Lenis({
+  duration: 1.5,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  direction: "vertical",
+  gestureDirection: "vertical",
+  smooth: true,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
