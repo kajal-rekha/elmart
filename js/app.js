2686d361cf1ef8a1abb4eb7cc8e67a403d75a54e
kajal-2011 <kajalrekha888@gmail.com>
2024-02-21 08:40:31 +0600

    lenis remove

--- js/app.js
+++ js/app.js
@@ -94,19 +94,3 @@ $(".owl-carousel").owlCarousel({
   },
 });
 
-// smooth scroll
-
-const lenis = new Lenis({
-  duration: 1.5,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-  direction: "vertical",
-  gestureDirection: "vertical",
-  smooth: true,
-});
-
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
